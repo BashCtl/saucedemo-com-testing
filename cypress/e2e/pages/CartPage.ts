103f9797035812fd79dacabc6e8c8664b5205dc3
space <ki14.skakun@gmail.com>
2022-11-07 17:21:01 +0200

    product page in progress

---
+++ cypress/e2e/pages/CartPage.ts
@@ -0,0 +1,6 @@
+
+class CartPage {
+    private productName = () => cy.get('.inventory_item_name')
+}
+
+export default new CartPage()
--- cypress/e2e/pages/ProductsPage.ts
+++ cypress/e2e/pages/ProductsPage.ts
@@ -1,9 +1,22 @@
+/// <reference types="cypress-xpath" />
+
 class ProductsPage {
     private pageTitle = () => cy.get('.title')
+    private addProductBtn = (product) => cy.xpath(`//*[text()="${product}"]/ancestor::*[contains(@class,"item_label")]/following-sibling::*[@class="pricebar"]//button[contains(@id,"add")]`)
+    private shoppingCartIcon = () => cy.get('.shopping_cart_badge')
 
     checkPageTitle(title) {
         this.pageTitle().should('have.text', title)
         return this
+    }
+
+    addProductToCart(productName) {
+        this.addProductBtn(productName).click()
+        return this
+    }
+
+    openCart(){
+        this.shoppingCartIcon().click()
     }
 
 }
